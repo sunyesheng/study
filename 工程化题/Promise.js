d0167b81f826b32585463e261e5446667fc40fef
孙业盛 <1481633100@qq.com>
2023-11-09 00:07:41 +0800

    epc: 手写简单的promise 1/3

--- 工程化题/Promise.js
+++ 工程化题/Promise.js
@@ -1,2 +1,82 @@
 // 理解 Promise 承诺
-class MyPromise {}
+const PENDDING = 'pendding'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+
+class MyPromise {
+  #state = PENDDING
+  #value = undefined
+  #handlers = []
+  constructor(executor) {
+    try {
+      executor(this._resolev.bind(this), this._reject.bind(this))
+    } catch (e) {
+      this._reject.bind(this)(e)
+    }
+  }
+
+  _resolev(data) {
+    this.#changeState(FULFILLED, data)
+  }
+
+  _reject(reason) {
+    this.#changeState(REJECTED, reason)
+  }
+
+  #changeState(state, data) {
+    if (this.#state !== PENDDING) return
+    this.#state = state
+    this.#value = data
+    this.#run()
+  }
+
+  then(onFulfilled, onRejected) {
+    return new MyPromise((res, rej) => {
+      this.#handlers.push({
+        onFulfilled,
+        onRejected,
+        res,
+        rej,
+      })
+      this.#run()
+    })
+  }
+  #run() {
+    if (this.#state === PENDDING) return
+    while (this.#handlers.length > 0) {
+      const { onFulfilled, onRejected, res, rej } = this.#handlers.shift()
+      if (this.#state === FULFILLED) {
+        onFulfilled(this.#value)
+      }
+      if (this.#state === REJECTED) {
+        onRejected(this.#value)
+      }
+    }
+  }
+}
+
+const p = new MyPromise((res, rej) => {
+  setTimeout(() => {
+    res('node')
+  }, 1000)
+})
+
+p.then(
+  (res) => {
+    console.log('res1', res)
+  },
+  (rej) => {
+    console.log('rej1', rej)
+  }
+)
+
+p.then(
+  (res) => {
+    console.log('res2', res)
+  },
+  (rej) => {
+    console.log('rej2', rej)
+  }
+)
+
+console.log('first', p)
