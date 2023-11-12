058a932000c8dfaccf1380d18e08624845f327ca
孙业盛 <1481633100@qq.com>
2023-11-12 23:56:09 +0800

    eazy: 相交链表

---
+++ 算法题/相较链表.js
@@ -0,0 +1,30 @@
+// https://leetcode.cn/problems/intersection-of-two-linked-lists/?envType=study-plan-v2&envId=top-100-liked
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode1 = function (headA, headB) {
+  const visited = new Set()
+  let temp = headA
+  while (temp !== null) {
+    visited.add(temp)
+    temp = temp.next
+  }
+  temp = headB
+  while (temp !== null) {
+    if (visited.has(temp)) {
+      return temp
+    }
+    temp = temp.next
+  }
+  return null
+}
