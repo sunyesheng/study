adc39d401ea90059f857ae6763f612cd57140b2c
孙业盛 <1481633100@qq.com>
2023-11-13 23:29:47 +0800

    eazy: 是否是回文链表

--- 算法题/回文链表.js
+++ 算法题/回文链表.js
@@ -30,3 +30,29 @@ var isPalindrome = function (head) {
   frontPointer = head
   return recursivelyCheck(head)
 }
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome1 = function (head) {
+  const array = []
+  let current = head
+  while (current !== null) {
+    array.push(current.val)
+    current = current.next
+  }
+  for (let i = 0, j = array.length - 1; i <= j; i++, j--) {
+    if (array[i] !== array[j]) {
+      return false
+    }
+  }
+  return true
+}
