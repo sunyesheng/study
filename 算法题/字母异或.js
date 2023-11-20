7a870ca12b79637b2ef51c10eae1af46534a47e8
孙业盛 <1481633100@qq.com>
2023-11-20 23:43:08 +0800

    middle: 字母异位词分组

--- 算法题/字母异或.js
+++ 算法题/字母异或.js
@@ -2,11 +2,26 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function (strs) {
+var groupAnagrams1 = function (strs) {
   const hash = {}
   for (let str of strs) {
     let map = Array.from(str).sort().join('')
     hash[map] = hash[map] ? [...hash[map], str] : [str]
+  }
+  return Object.values(hash)
+}
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams2 = function (strs) {
+  const hash = {}
+  for (let str of strs) {
+    let key = new Array(26).fill(0)
+    for (let s of str) {
+      key[s.charCodeAt() - 'a'.charCodeAt()]++
+    }
+    hash[key] = hash[key] ? [...hash[key], str] : [str]
   }
   return Object.values(hash)
 }
